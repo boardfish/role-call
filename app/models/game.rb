4fc1fd234825a21382d43ae9271823f4f2ea6e98
Simon Fish <si@mon.fish>
2018-11-06 21:29:12 +0000

    Add function to determine available role types

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,11 +5,36 @@ class Game < ApplicationRecord
   has_many :users, through: :game_sessions
   belongs_to :game_master, class_name: 'User'
 
+  DEFAULT_QUOTAS = {
+    "Town": {
+      "Killing": 2,
+      "Investigative": 2,
+      "Protective": 1,
+      "Support": 1,
+      "Random": 3
+    },
+    "Mafia": {
+      "Random": 2,
+      "Godfather": 1,
+      "Mafioso": 1
+    },
+    "Neutral": {
+      "Evil": 1,
+      "Killing": 1
+    }
+  }.deep_symbolize_keys
+
+
+
   def roles
-    game_sessions
+    sorted_roles = game_sessions
       .map(&:role)
       .group_by(&:group).map do |group, roles|
       { "#{group}": roles.group_by(&:role_type) }
-    end
+    end[0].deep_symbolize_keys
+  end
+
+  def available_roles
+    DEFAULT_QUOTAS.deep_merge(roles) { |group, quota, count| quota - count.count}
   end
 end
