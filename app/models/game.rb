62c6796d1bccc0cb3cba9282de7d0e843d3db165
Simon Fish <si@mon.fish>
2018-11-06 20:37:19 +0000

    Add roles function
    This outputs a hash that sorts all roles into their types and groups.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 class Game < ApplicationRecord
   has_many :game_sessions
   has_many :users, through: :game_sessions
   belongs_to :game_master, class_name: 'User'
+
+  def roles
+    game_sessions
+      .map(&:role)
+      .group_by(&:group).map do |group, roles|
+      { "#{group}": roles.group_by(&:role_type) }
+    end
+  end
 end
